234a6e1bbd9ad4fac0ce3600494961b039e82ba6
jiangyukai <jiangyukai>
2018-08-13 08:55:01 +0800

    first Springboot

---
+++ src/main/java/SampleController.java
@@ -0,0 +1,32 @@
+/**
+ * @Auther: yk
+ * @Date: 2018-8-10 17:01
+ * @Description:
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *@className SampleController
+ *@Description TODO
+ *@Author yk
+ *@Date 2018-8-10 17:01
+ *@Version 1.0
+ **/
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
